e553634e180530249ba8f17d7b45b33fb6feec3b
Aryan Arora <aryan.arora180@gmail.com>
2022-03-05 00:14:40 +0530

    Added routes for requests

--- apps/spaghetti/src/routes/index.ts
+++ apps/spaghetti/src/routes/index.ts
@@ -1,5 +1,6 @@
 import authRouter from './auth';
 import testRouter from './test';
 import userRouter from './user';
+import requestRouter from './request';
 
-export { authRouter, testRouter, userRouter };
+export { authRouter, testRouter, userRouter, requestRouter };
